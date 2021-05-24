3915274a3a787d314f13dd23fb7bd68b1b957c3a
leonardoprimieri <leonardoprimieri>
2021-05-23 22:20:22 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = require('./routes');
 const server = express();
+const cors = require('cors');
 
 server.use(express.urlencoded({ extended: true }));
-
+server.use(cors());
 server.use(express.json());
 server.use(router);
 
