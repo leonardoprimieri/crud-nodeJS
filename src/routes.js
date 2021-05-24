680df656685ae7f1d895efde301fb949dd02a34c
leonardoprimieri <leonardoprimieri>
2021-05-23 22:18:00 -0300

    a

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const user = require("./controllers/user");
+const user = require('./controllers/user');
 
-router.get("/", user.index);
-router.get("/user/create", user.create);
-router.post("/user/create", user.createAction);
-
-router.get("/user/:_id/edit", user.edit);
-router.post("/user/:_id/edit", user.editAction);
-
-router.get("/user/:_id", user.show);
-
-router.get("/user/:_id/delete", user.delete);
+router.post('/test', user.index);
 
 module.exports = router;
